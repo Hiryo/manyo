517e0eeee8fdc6b5f71f98968ef98eb3370a9373
Hirosaki Ryo <rih60909@gmail.com>
2021-05-05 19:22:27 +0900

    migrated null_task

--- db/migrate/20210505095041_change_column_null_tasks.rb
+++ db/migrate/20210505095041_change_column_null_tasks.rb
@@ -1,6 +1,3 @@
 class ChangeColumnNullTasks < ActiveRecord::Migration[5.2]
-  def change
-    create_table :tasks do |t|
-      t.string :name, null: false
-      t.string :detail, null: false
+  change_column :tasks, :detail, :string, null: false
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_073310) do
+ActiveRecord::Schema.define(version: 2021_05_05_095041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
-    t.string "detail"
+    t.string "detail", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
