386e5f1a836b9477bf7c06a8bf22195c308fe9a5
Hirosaki Ryo <rih60909@gmail.com>
2021-05-07 22:40:01 +0900

    submit

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   let!(:task) { FactoryBot.create(:task, detail: 'task') }
+  let!(:second_task) { FactoryBot.create(:second_task, detail: 'task2') }
   before do
     visit tasks_path
   end
@@ -19,22 +20,22 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
         task = FactoryBot.create(:task, name: 'task')
-        visit tasks_path
         expect(page).to have_content 'task'
       end
     end
-  end
 
   context 'タスクが作成日時の降順に並んでいる場合' do
     it '新しいタスクが一番上に表示される' do
+      # binding.irb
       task_list = all('.task_row')
       expect(task_list[0]).to have_content 'task'
     end
   end
+end
+
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-				# binding.irb
 				task = FactoryBot.create(:task, name: 'task', detail: 'task')
 				visit task_path(task.id)
 				expect(page).to have_content 'task'
