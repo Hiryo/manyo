4c19124c1cf8bd9e5aab01fb5467dd8697801bb3
Hirosaki Ryo <rih60909@gmail.com>
2021-05-07 20:43:04 +0900

    added sort test

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,31 +1,10 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-<<<<<<< HEAD
   let!(:task) { FactoryBot.create(:task, detail: 'task') }
   before do
     visit tasks_path
   end
 
-    describe '一覧表示機能' do
-      context '一覧画面に遷移した場合' do
-        it '作成済みのタスク一覧が表示される' do
-          task = FactoryBot.create(:task, name: 'task')
-          visit tasks_path
-          expect(page).to have_content 'task'
-      end　
-    end
-      context 'タスクが作成日時の降順に並んでいる場合'　do
-        it '新しいタスクが一番上に表示される' do
-          task_list = all('.task_row')
-          expect(task_list[0]).to have_content ''
-          expect(task_list[1]).to have_content ''
-    end
-  end
-=======
-	before do
-    FactoryBot.create(:task)
-    FactoryBot.create(:second_task)
-  end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -45,6 +24,13 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
+
+  context 'タスクが作成日時の降順に並んでいる場合' do
+    it '新しいタスクが一番上に表示される' do
+      task_list = all('.task_row')
+      expect(task_list[0]).to have_content 'task'
+    end
+  end
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
@@ -55,5 +41,4 @@ RSpec.describe 'タスク管理機能', type: :system do
        end
      end
   end
->>>>>>> 277891f78570df6c27c792c2c86aac18a4a53f27
 end
