13f1221267829f5df2f0b6860efccadd5c0ceab4
Hirosaki Ryo <rih60909@gmail.com>
2021-05-14 01:28:28 +0900

    fixed to migration file

--- db/migrate/20210512104029_add_column_to_tasks_priority.rb
+++ db/migrate/20210512104029_add_column_to_tasks_priority.rb
@@ -1,4 +1,4 @@
-class AddColumnToTasks < ActiveRecord::Migration[5.2]
+class AddColumnToTasksPriority < ActiveRecord::Migration[5.2]
   def change
     add_column :tasks, :priority, :integer, default: 0
   end
