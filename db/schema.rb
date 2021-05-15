cdeac5af76d4b1852c5adebd94a39eeb19923744
Hirosaki Ryo <rih60909@gmail.com>
2021-05-15 17:40:05 +0900

    added external key for tasks migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_063805) do
+ActiveRecord::Schema.define(version: 2021_05_15_083603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,9 @@ ActiveRecord::Schema.define(version: 2021_05_15_063805) do
     t.integer "status", default: 0
     t.datetime "expired_at"
     t.integer "priority", default: 0
+    t.bigint "user_id"
     t.index ["name"], name: "index_tasks_on_name"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_05_15_063805) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
