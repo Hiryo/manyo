210e7d751772cac1d98568f52001f3a739bb9566
Hirosaki Ryo <rih60909@gmail.com>
2021-05-15 20:07:40 +0900

    added edit for my acount

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,7 +14,7 @@ class TasksController < ApplicationController
     elsif params[:priority]
       @tasks = Task.order(:priority)
     else
-      @tasks = Task.all
+      @tasks = current_user.tasks.all
     end
 
     @tasks = @tasks.page(params[:page]).per(2)
@@ -38,6 +38,8 @@ class TasksController < ApplicationController
   end
 
   def show
+    # @task = current_user.tasks.find_by(task_id: @task.id)
+
   end
 
   def edit
@@ -63,7 +65,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:name, :detail, :expired_at, :status, :priority)
+    params.require(:task).permit(:name, :detail, :expired_at, :status, :priority, :user_id)
   end
 
   def set_task
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
       render :new
@@ -15,6 +16,19 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   private
