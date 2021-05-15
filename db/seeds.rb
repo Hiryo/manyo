eaaff789535527f0c07fb22565b971ffcdc1b72f
Hirosaki Ryo <rih60909@gmail.com>
2021-05-15 23:08:58 +0900

    created admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "hilary",
+            email: "hill@dic.com",
+            password: "123123",
+            password_confirmation: "123123",
+            admin: true)
